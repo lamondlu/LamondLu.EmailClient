400a706011e33cbd4ce927f29c8be33e6f7859b7
Lamond Lu <lunan@shintechchina.com>
2020-04-07 09:32:05 +0800

    update the code

---
+++ src/LamondLu.EmailClient.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LamondLu.EmailClient.ConsoleApp
+{
+    class Program
+    {
+        private const string _appsettings = "appsettings.json";
+
+        private static async Task Main(string[] args)
+        {
+            IHost host = new HostBuilder().ConfigureAppConfiguration((hostContext, configApp) =>
+            {
+                configApp.SetBasePath(Directory.GetCurrentDirectory());
+                configApp.AddJsonFile(_appsettings, optional: true);
+                configApp.AddJsonFile(
+                    $"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
+                    optional: true);
+                configApp.AddCommandLine(args);
+            }).ConfigureServices((hostContext, services) =>
+            {
+
+
+            }).UseConsoleLifetime().Build();
+
+            await host.RunAsync();
+        }
+    }
+}
