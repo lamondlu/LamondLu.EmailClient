bfcc655f06d148c0a1975493b69aca4a98784a4e
Lamond Lu <lunan@shinetechsoftware.com>
2020-04-08 08:16:42 +0800

    add concreat class

---
+++ src/LamondLu.EmailClient.Infrastructure.EmailService.Mailkit/ReplyRuleProcessor.cs
@@ -0,0 +1,13 @@
+using LamondLu.EmailClient.Domain;
+using LamondLu.EmailClient.Domain.Interface;
+
+namespace LamondLu.EmailClient.Infrastructure.EmailService.Mailkit
+{
+    public class ReplyRuleProcessor : IRuleProcessor
+    {
+        public void Run(Email email)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
