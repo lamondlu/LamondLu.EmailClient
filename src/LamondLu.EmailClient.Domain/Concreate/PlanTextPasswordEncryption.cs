5fded15fe0a4ebd17494a9c2d5bf929c83f226dc
lamondlu <309728709@qq.com>
2020-04-13 09:28:34 +0800

    add password encryption interface and a default plain text implementation

---
+++ src/LamondLu.EmailClient.Domain/Concreate/PlanTextPasswordEncryption.cs
@@ -0,0 +1,15 @@
+﻿using LamondLu.EmailClient.Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LamondLu.EmailClient.Domain.Concreate
+{
+    public class PlanTextPasswordEncryption : IPasswordEncryption
+    {
+        public string Encrypt(string password)
+        {
+            return password;
+        }
+    }
+}
---
+++ src/LamondLu.EmailClient.Domain/ViewModels/EmailConnectorConfigViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LamondLu.EmailClient.Domain.ViewModels
+{
+    public class EmailConnectorConfigViewModel
+    {
+        public Guid EmailConnectorId { get; set; }
+
+        public string Name { get; set; }
+
+        public string IP { get; set; }
+
+        public int Port { get; set; }
+
+        public bool EnableSSL { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
