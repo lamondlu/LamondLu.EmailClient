7b4736648d8c19b6ff6c0b06f775a0edd34f9608
lamondlu <309728709@qq.com>
2020-04-13 08:46:52 +0800

    add new email connector repository

---
+++ src/LamondLu.EmailClient.Infrastructure.DataPersistent/EmailConnectorRepository.cs
@@ -0,0 +1,11 @@
+﻿using LamondLu.EmailClient.Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LamondLu.EmailClient.Infrastructure.DataPersistent
+{
+    public class EmailConnectorRepository : IEmailConnectorRepository
+    {
+    }
+}
