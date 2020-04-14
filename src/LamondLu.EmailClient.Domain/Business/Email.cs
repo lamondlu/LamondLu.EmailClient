6ca2ea41b16e0f9d37dbbef21d5e3842a47b6b73
lamondlu <309728709@qq.com>
2020-04-14 08:19:54 +0800

    add email folder concept

--- src/LamondLu.EmailClient.Domain/Business/Email.cs
+++ src/LamondLu.EmailClient.Domain/Business/Email.cs
@@ -14,6 +14,8 @@ namespace LamondLu.EmailClient.Domain
 
         public Guid EmailId { get; private set; }
 
+        public EmailFolder EmailFolder { get; private set; }
+
         public DateTime ReceivedDate { get; set; }
 
         public EmailAddress Sender { get; set; }
---
+++ src/LamondLu.EmailClient.Domain/Business/EmailFolder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LamondLu.EmailClient.Domain
+{
+    public class EmailFolder
+    {
+        public string FolderName { get; set; }
+
+        public List<EmailFolder> SubFolders { get; set; }
+    }
+}
