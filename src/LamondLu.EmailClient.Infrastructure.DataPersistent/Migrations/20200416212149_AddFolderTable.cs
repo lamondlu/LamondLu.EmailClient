0f49b28666555cf1688b916a6c8c0035bdcaad7c
lamondlu <309728709@qq.com>
2020-04-17 07:52:59 +0800

    add new table migration

---
+++ src/LamondLu.EmailClient.Infrastructure.DataPersistent/Migrations/20200416212149_AddFolderTable.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace LamondLu.EmailClient.Infrastructure.DataPersistent.Migrations
+{
+    [Migration(20200416212149)]
+    public class AddFolderTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("EmailFolder")
+               .WithColumn("FolderId").AsGuid().PrimaryKey()
+               .WithColumn("FolderName").AsString().NotNullable()
+               .WithColumn("FolderFullPath").AsString().NotNullable()
+               .WithColumn("ParentId").AsGuid().ForeignKey("FK_Folder_ParentId_Folder_FolderId", "EmailFolder", "FolderId")
+               .WithColumn("IsDeleted").AsBoolean().NotNullable().WithDefaultValue(false);
+        }
+
+        public override void Down()
+        {
+            Delete.Table("EmailFolder");
+        }
+    }
+}
---
+++ src/LamondLu.EmailClient.Infrastructure.DataPersistent/Migrations/20200416212645_AddEmailTable.cs
@@ -0,0 +1,35 @@
+﻿using FluentMigrator;
+
+namespace LamondLu.EmailClient.Infrastructure.DataPersistent.Migrations
+{
+    [Migration(20200416212645)]
+    public class AddEmailTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Email")
+                .WithColumn("EmailId").AsGuid().PrimaryKey()
+                .WithColumn("MessageId").AsString().NotNullable()
+                .WithColumn("MailkitId").AsInt32().Nullable()
+                .WithColumn("MailkitValidityId").AsInt32().Nullable()
+                .WithColumn("ReceivedDate").AsDateTime().Nullable()
+                .WithColumn("SentDate").AsDateTime().Nullable()
+                .WithColumn("ImportedDate").AsDateTime().NotNullable()
+                .WithColumn("Subject").AsString().Nullable()
+                .WithColumn("Body").AsCustom("longtext").Nullable()
+                .WithColumn("TextBody").AsCustom("longtext").Nullable()
+                .WithColumn("Notes").AsCustom("longtext").Nullable()
+                .WithColumn("SenderAddress").AsString().NotNullable()
+                .WithColumn("SenderDisplayName").AsString().Nullable()
+                .WithColumn("ReceiptAddress").AsString().NotNullable()
+                .WithColumn("ReceiptDisplayName").AsString().Nullable()
+                .WithColumn("FolderId").AsGuid().ForeignKey("FK_Email_FolderId_EmailFolder_FolderId", "EmailFolder", "FolderId");
+        }
+
+        public override void Down()
+        {
+            Delete.ForeignKey("FK_Email_FolderId_EmailFolder_FolderId").OnTable("Email");
+            Delete.Table("Email");
+        }
+    }
+}
