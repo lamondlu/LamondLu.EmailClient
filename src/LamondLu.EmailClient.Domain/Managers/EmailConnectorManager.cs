915ac193c90f20e055542379c66baf0979025f61
lamondlu <309728709@qq.com>
2020-04-21 10:39:53 +0800

    add method for email connector

--- src/LamondLu.EmailClient.Domain/Managers/EmailConnectorManager.cs
+++ src/LamondLu.EmailClient.Domain/Managers/EmailConnectorManager.cs
@@ -6,5 +6,29 @@ namespace LamondLu.EmailClient.Domain.Managers
 {
     public class EmailConnectorManager : ManagerBase
     {
+        public void Add()
+        {
+
+        }
+
+        public void Update(Guid emailConnectorId)
+        {
+
+        }
+
+        public void Delete(Guid emailConnectorId)
+        {
+
+        }
+
+        public void Stop(Guid emailConnectorId)
+        {
+
+        }
+
+        public void Start(Guid emailConnectorId)
+        {
+
+        }
     }
 }
