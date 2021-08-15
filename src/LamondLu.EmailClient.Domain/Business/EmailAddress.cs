9d235d31ae85bc82ddbbec37ef152045c24cc7f0
Lamond Lu <lunan@shinetechchina.com>
2021-08-15 18:54:50 +0800

    Add code

--- src/LamondLu.EmailClient.Domain/Business/EmailAddress.cs
+++ src/LamondLu.EmailClient.Domain/Business/EmailAddress.cs
@@ -2,6 +2,15 @@
 {
     public class EmailAddress
     {
+        public EmailAddress(string address, string displayName){
+            Address = address;
+            DisplayName = displayName;
+        }
+
+        public EmailAddress(string address){
+            Address = address;
+        }
+
         public string Address { get; set; }
 
         public string DisplayName { get; set; }
