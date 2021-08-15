0fec971bd442a5fdacec4db679f1718a7115208c
lamondlu <309728709@qq.com>
2021-08-15 21:53:39 +0800

    Add new table for email address and email body

---
+++ src/LamondLu.EmailClient.ConsoleApp/Migrations/20210815214847_AddEmailAddressTable.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+
+namespace LamondLu.EmailClient.ConsoleApp.Migrations
+{
+    [Migration(20210815214847)]
+    public class AddEmailAddressTable : Migration
+    {
+        public override void Up()
+        {
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ src/LamondLu.EmailClient.Infrastructure.DataPersistent/Migrations/20210815214908_AddEmailAddressTable.cs
@@ -0,0 +1,30 @@
+﻿using FluentMigrator;
+
+namespace LamondLu.EmailClient.Infrastructure.DataPersistent.Migrations
+{
+    [Migration(20210815214908)]
+    public class AddEmailAddressTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("EmailAddress")
+                .WithColumn("EmailAddressId").AsGuid().PrimaryKey()
+                .WithColumn("Name").AsString()
+                .WithColumn("Address").AsString();
+
+            Create.Table("EmailContent")
+                .WithColumn("EmailId").AsGuid().PrimaryKey()
+                .WithColumn("TextBody").AsCustom("text")
+                .WithColumn("Body").AsCustom("text");
+
+            Create.ForeignKey("EmailContent_EmailId_Email_Id").FromTable("EmailContent").ForeignColumn("EmailId").ToTable("Email").PrimaryColumn("Id");
+        }
+
+        public override void Down()
+        {
+            Delete.Table("EmailAddress");
+            Delete.ForeignKey("EmailContent_EmailId_Email_Id");
+            Delete.Table("EmailContent");
+        }
+    }
+}
