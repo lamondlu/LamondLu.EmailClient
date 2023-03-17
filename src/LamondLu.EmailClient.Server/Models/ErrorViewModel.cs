82d63935c89b38478b5596707208dbd0191c34a2
lunan <lunan198506@hotmail.com>
2023-03-17 18:56:55 +0800

    update the code

--- src/LamondLu.EmailClient.Server/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LamondLu.EmailClient.Server.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
