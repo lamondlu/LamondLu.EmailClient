3fe0fe4c83805ff041256525ac213ae719967a72
lunan <309728709@qq.com>
2023-05-05 07:37:12 +0800

    Update the code

--- src/LamondLu.EmailX.Portal/Controllers/EmailConnectorController.cs
+++ src/LamondLu.EmailX.Portal/Controllers/EmailConnectorController.cs
@@ -20,8 +20,9 @@ namespace LamondLu.EmailX.Portal.Controllers
             _unitOfWork = unitOfWorkFactory.Create();
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(Guid? emailConnectorId = null)
         {
+            ViewBag.EmailConnectorId = emailConnectorId;
             var emailConnectors = await _unitOfWork.EmailConnectorRepository.GetEmailConnectors();
 
             return View(emailConnectors);
